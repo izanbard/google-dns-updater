8fc917e69c07d7091f08d2cc1c2fa6ad56c59185
Gordy Wills <gordy.wills@gmail.com>
2024-07-07 21:17:29 +0100

    looping the execution

--- updater/__init__.py
+++ updater/__init__.py
@@ -1,5 +1,10 @@
 import logging
+import sys
 
 logger = logging.getLogger("updater")
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s")
+logHandler = logging.StreamHandler(sys.stdout)
+logHandler.setFormatter(logFormatter)
 logger.setLevel(logging.INFO)
+logger.addHandler(logHandler)
 logger.info("Starting updater...")
